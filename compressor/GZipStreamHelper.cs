a6d869f0fb74a641ace596cdf96a023db80cf70a
Andrey Krivosheya <rtti@bk.ru>
2020-10-19 11:23:40 +0300

    Implemented threaded compress/uncompress: Refactored GZipStreamHelper to safely target other .net framewrok versions

--- compressor/GZipStreamHelper.cs
+++ compressor/GZipStreamHelper.cs
@@ -3,6 +3,7 @@ namespace compressor
     static class GZipStreamHelper
     {
         public static readonly byte[] Header = new byte[] {
+#if NET_CORE_3_1
             /*magic*/
             0x1f, 0x8b,
             /*deflate*/
@@ -11,6 +12,10 @@ namespace compressor
             0x00,
             /*file modification time in Unix format*/
             0x00, 0x00, 0x00, 0x00
+#else
+            /*magic*/
+            0x1f, 0x8b
+#endif
         };
     }
 }
